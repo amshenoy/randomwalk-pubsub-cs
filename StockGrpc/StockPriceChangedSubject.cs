4b96b0441441a1778b25df7f8ec81dca2c87b8f3
Abhishek Shenoy <abhishekshenoy@hotmail.co.uk>
2024-08-25 21:16:21 +0100

    Make event notifier threadsafe

--- StockGrpc/StockPriceChangedSubject.cs
+++ StockGrpc/StockPriceChangedSubject.cs
@@ -4,14 +4,19 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using StockGrpc;
 
+// TODO: Rename this to EventNotifier and use generics if needed
+// Move the listener class to here
+
 public class StockPriceChangedSubject
 {
-    private readonly ConcurrentDictionary<string, List<IServerStreamWriter<StockPriceResponse>>> _observers = new();
+    // Changed List to ConcurrentBag for thread safety
+    private readonly ConcurrentDictionary<string, ConcurrentBag<IServerStreamWriter<StockPriceResponse>>> _observers = new();
 
     public void Register(string symbol, IServerStreamWriter<StockPriceResponse> observer)
     {
-        _observers.AddOrUpdate(symbol, [observer],
-            (key, existingObservers) =>
+        _observers.AddOrUpdate(symbol,
+            _ => [observer],
+            (_, existingObservers) =>
             {
                 existingObservers.Add(observer);
                 return existingObservers;
@@ -22,8 +27,16 @@ public class StockPriceChangedSubject
     {
         if (_observers.TryGetValue(symbol, out var observers))
         {
-            observers.Remove(observer);
-            if (observers.Count == 0)
+            // Remove the observer
+            bool removed = observers.TryTake(out var existingObserver);
+            while (!removed)
+            {
+                // In case observer is not found in the bag (unlikely but possible), retry
+                removed = observers.TryTake(out existingObserver);
+            }
+
+            // Remove the symbol entry if there are no observers left
+            if (observers.IsEmpty)
             {
                 _observers.TryRemove(symbol, out _);
             }
@@ -38,13 +51,11 @@ public class StockPriceChangedSubject
             var response = new StockPriceResponse { Symbol = eventObj.Symbol, Price = eventObj.Price };
             foreach (var observer in observers)
             {
-				try {
-                	await observer.WriteAsync(response);
-				}
-				catch (Exception ex)
-				{
-					Console.WriteLine(ex.ToString());
-				}
+                try
+                {
+                    await observer.WriteAsync(response);
+                }
+                catch (Exception) {} // Connection closed
             }
         }
     }
