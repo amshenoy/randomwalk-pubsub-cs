29768173a3d7d406cd661a456203bae9a093c72b
Abhishek Shenoy <abhishekshenoy@hotmail.co.uk>
2024-08-27 22:18:45 +0100

    Move StockRepository to repositories directory

---
+++ StockGrpc/Repositories/StockRepository.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+public sealed class StockRepository
+{
+    private readonly ConcurrentDictionary<string, Stock> _stocks = new();
+
+    public StockRepository()
+    {
+        var random = new Random();
+        AddStock(new Stock("GOOGL", random.Next(50, 200)));
+        AddStock(new Stock("AMZN", random.Next(50, 200)));
+        AddStock(new Stock("MSFT", random.Next(50, 200)));
+        AddStock(new Stock("AAPL", random.Next(50, 200)));
+        AddStock(new Stock("NFLX", random.Next(50, 200)));
+    }
+
+    public void AddStock(Stock stock)
+    {
+        _stocks[stock.Symbol] = stock;
+    }
+
+    public IReadOnlyCollection<Stock> GetStocks()
+    {
+        return _stocks.Values.ToList().AsReadOnly();
+    }
+
+    public Stock? GetStock(string symbol)
+    {
+        return _stocks.TryGetValue(symbol, out var stock) ? stock : null;
+    }
+}
+
+public record StockPriceChangedEvent(string Symbol, double Price);
+
+public class Stock(string symbol, double price)
+{
+    private readonly string _symbol = symbol;
+    private double _price = price;
+
+	public string Symbol => _symbol;
+    public double Price => _price;
+
+    public void UpdatePrice(double price)
+    {
+        _price = price;
+        DomainEvents.Publish(new StockPriceChangedEvent(_symbol, _price));
+    }
+}
