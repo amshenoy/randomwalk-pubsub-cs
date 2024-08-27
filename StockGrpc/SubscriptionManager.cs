07d9e6e2569b55be49cf01b0f6a28fcbeb93eead
Abhishek Shenoy <abhishekshenoy@hotmail.co.uk>
2024-08-27 22:17:10 +0100

    Use generics for creating and broadcasting to subscriptions on stream

---
+++ StockGrpc/SubscriptionManager.cs
@@ -0,0 +1,49 @@
+using Grpc.Core;
+using System.Collections.Concurrent;
+using StockGrpc;
+
+public class SubscriptionManager<Category, Event, Response>
+{
+    // Using ConcurrentDictionary with IServerStreamWriter as key and byte as value (acting like a HashSet)
+    private readonly ConcurrentDictionary<Category, ConcurrentDictionary<IServerStreamWriter<Response>, byte>> _observers = new();
+
+	private readonly Func<Event, Category> _eventCategoryMapper;
+	private readonly Func<Event, Response> _eventResponseMapper;
+
+	public SubscriptionManager(Func<Event, Category> eventCategoryMapper, Func<Event,Response> eventResponseMapper)
+	{
+		_eventCategoryMapper = eventCategoryMapper;
+		_eventResponseMapper = eventResponseMapper;
+	}
+
+    public void Register(Category category, IServerStreamWriter<Response> observer)
+    {
+        var observers = _observers.GetOrAdd(category, _ => new());
+        observers[observer] = 0;
+    }
+
+    public void Unregister(Category category, IServerStreamWriter<Response> observer)
+	{
+		if (!_observers.TryGetValue(category, out var observers)) return;
+		observers.TryRemove(observer, out _);
+		if (!observers.IsEmpty) return;
+		_observers.TryRemove(category, out _);
+	}
+
+	public void Broadcast(Event eventObj)
+    {
+		var category = _eventCategoryMapper(eventObj);
+        if (_observers.TryGetValue(category, out var observers))
+        {
+            var response = _eventResponseMapper(eventObj);
+            foreach (var observer in observers.Keys)
+            {
+                try
+                {
+					_ = Task.Run(() => observer.WriteAsync(response));
+                }
+                catch (Exception) {} // Connection closed
+            }
+        }
+    }
+}
