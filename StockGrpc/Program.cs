d15c7694cfa7191b1d2e652906266b8f7ca65bea
Abhishek Shenoy <abhishekshenoy@hotmail.co.uk>
2024-08-27 22:19:46 +0100

    Refactoring and cleanup for new design

--- StockGrpc/Program.cs
+++ StockGrpc/Program.cs
@@ -1,25 +1,34 @@
+using StockGrpc;
 using StockGrpc.Services;
+
+using StockPriceSubscriptionManager = SubscriptionManager<string, StockPriceChangedEvent, StockGrpc.StockPriceResponse>;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<StockRepository>();
 builder.Services.AddHostedService<RandomStockPriceUpdatingService>();
-builder.Services.AddSingleton<StockPriceChangedSubject>();
-builder.Services.AddSingleton<StockPriceChangedEventListener>();
+builder.Services.AddSingleton<StockPriceSubscriptionManager>(
+	provider =>
+    {
+		static string eventCategoryMapper(StockPriceChangedEvent e) => e.Symbol;
+		static StockPriceResponse eventResponseMapper(StockPriceChangedEvent e) => new(){ Symbol = e.Symbol, Price = e.Price };
+		return new(eventCategoryMapper, eventResponseMapper);
+    }
+);
 
 builder.Services.AddGrpc();
 
 var app = builder.Build();
 
 // app.UseMiddleware<GrpcErrorHandlerMiddleware>();
-app.MapGrpcService<StockService>();
+app.MapGrpcService<StockGrpc.Services.StockService>();
 app.MapGet("/", () => """
 Communication with gRPC endpoints must be made through a gRPC client.
 To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909
 """);
 
-var eventListener = app.Services.GetRequiredService<StockPriceChangedEventListener>();
-DomainEvents.Subscribe<StockPriceChangedEvent>(eventListener.HandleEvent);
+var eventListener = app.Services.GetRequiredService<StockPriceSubscriptionManager>();
+DomainEvents.Subscribe<StockPriceChangedEvent>(eventListener.Broadcast);
 
 Console.WriteLine("Starting App");
 app.Run();
